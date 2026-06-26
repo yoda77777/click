b453935a70bd12bbe4a1bad1b44fb971d6d6d394
Memduh Çelik <memduhcelik33@gmail.com>
2026-06-26 16:47:47 +0300

    test: support exception notes on Python 3.10

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,6 +1,13 @@
+import sys
+
+import pytest
+
 import click
 
 
+@pytest.mark.skipif(
+    sys.version_info < (3, 11), reason="Exception.add_note requires Python 3.11+"
+)
 def test_clickexception_show_includes_notes(capsys):
     """ClickException.show() displays exception notes (PEP 678).
 
@@ -9,6 +16,17 @@ def test_clickexception_show_includes_notes(capsys):
     exc = click.ClickException("foo")
     exc.add_note("bar")
     exc.add_note("baz")
+    exc.show()
+    err = capsys.readouterr().err
+    assert "Error: foo" in err
+    assert "bar" in err
+    assert "baz" in err
+
+
+def test_clickexception_show_includes_notes_attr(capsys):
+    """Notes are also honored when set via __notes__ (works on 3.10+)."""
+    exc = click.ClickException("foo")
+    exc.__notes__ = ["bar", "baz"]  # type: ignore[attr-defined]
     exc.show()
     err = capsys.readouterr().err
     assert "Error: foo" in err
